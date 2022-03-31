cc3725bca04fa6ce8838395471e7a053e4992c37
m-ganji <Mohamad.ganjif@gmail.com>
2022-03-31 16:23:49 +0430

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR KEY>';
+export const KEY = '2e8b5843-67d1-4f91-afe4-1bfc3a8f43af';
 export const MODAL_CLOSE_SEC = 2.5;
